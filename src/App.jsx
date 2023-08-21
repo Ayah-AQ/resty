8d5b515c9b63216fa4727f1240baaa76216773b9
Ayah-AQ <ayoya_1998@hotmail.com>
2023-08-21 12:44:20 +0000

    lab26

--- src/App.jsx
+++ src/App.jsx
@@ -15,73 +15,73 @@ import Form from './Components/Form';
 // import Form from './Components/Form';
 // import Results from './Components/Results';
 
-// class App extends React.Component {
+class App extends React.Component {
 
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       data: null,
-//       requestParams: {},
-//     };
-//   }
-
-//   callApi = (requestParams) => {
-//     // mock output
-//     const data = {
-//       count: 2,
-//       results: [
-//         {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-//         {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-//       ],
-//     };
-//     this.setState({data, requestParams});
-//   }
-
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <Header />
-//         <div>Request Method: {this.state.requestParams.method}</div>
-//         <div>URL: {this.state.requestParams.url}</div>
-//         <Form handleApiCall={this.callApi} />
-//         <Results data={this.state.data} />
-//         <Footer />
-//       </React.Fragment>
-//     );
-//   }
-// }
-
-function App() {
-
-  const [data, setData] = useState(null);
-  const [requestParams, setRequestParams] = useState({});
-
-  function callApi(requestParams) {
-    const newData = {
-      results: [
-        // {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-        // {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-        {method:requestParams.method},
-        {url:requestParams.url},
-        {response:requestParams.data},
-      ],
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      requestParams: {},
     };
-
-    setData(newData);
-    setRequestParams(requestParams);
   }
 
-  return (
-    <>
-      {/* {console.log(requestParams)} */}
-      <Header />
-      <div>Request Method: {requestParams.method}</div>
-      <div>URL: {requestParams.url}</div>
-      <Form handleApiCall={callApi} />
-      <Results data={data} />
-      <Footer />
-    </>
-  );
+  callApi = (requestParams) => {
+    // mock output
+    const data = {
+      count: 2,
+      results: [
+        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
+        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+      ],
+    };
+    this.setState({data, requestParams});
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <div>Request Method: {this.state.requestParams.method}</div>
+        <div>URL: {this.state.requestParams.url}</div>
+        <Form handleApiCall={this.callApi} />
+        <Results data={this.state.data} />
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
+
+// function App() {
+
+//   const [data, setData] = useState(null);
+//   const [requestParams, setRequestParams] = useState({});
+
+//   function callApi(requestParams) {
+//     const newData = {
+//       results: [
+//         // {name: 'fake thing 1', url: 'http://fakethings.com/1'},
+//         // {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+//         {method:requestParams.method},
+//         {url:requestParams.url},
+//         {response:requestParams.data},
+//       ],
+//     };
+
+//     setData(newData);
+//     setRequestParams(requestParams);
+//   }
+
+//   return (
+//     <>
+//       {/* {console.log(requestParams)} */}
+//       <Header />
+//       <div>Request Method: {requestParams.method}</div>
+//       <div>URL: {requestParams.url}</div>
+//       <Form handleApiCall={callApi} />
+//       <Results data={data} />
+//       <Footer />
+//     </>
+//   );
+// }
 
 export default App;
