e4d0ca7282f84c89e9e5e0ee6dbbbbb12f189731
Ayah-AQ <ayoya_1998@hotmail.com>
2023-08-21 17:08:19 +0000

    lab26

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,79 +1,34 @@
-// import React from 'react';
-import { useState } from "react";
-import "./Form.scss";
+import './Form.scss';
 
-function Form(props) {
-  const [input, setInput] = useState("");
-  const [method, setMethod] = useState("get");
-  const [textBox, setTextBox] = useState(false);
-  const [textBoxText, setTextBoxText] = useState({});
+function Form (props) {
 
-  function handleChange(e) {
-    setInput(e.target.value);
-  }
-  function handleData(e) {
-    setTextBoxText(e.target.value);
-  }
-  function handleClick(e) {
-    e.preventDefault();
-    setMethod(e.target.id);
-    if (e.target.id === "post" || e.target.id === "put") {
-      setTextBox(true);
-    } else {
-      setTextBox(false);
-    }
-  }
-  function handleSubmit(e) {
+ const handleSubmit = e => {
     e.preventDefault();
     const formData = {
-      method: method,
-      url: input,
-      data: textBoxText,
+      method:'GET',
+      url: 'https://pokeapi.co/api/v2/pokemon',
     };
-
-    props.handleApiCall(formData);
+    props.handleApiCall(formData); // check it later if props needed or not 
   }
-  
-  return (
-    <>
-    
-      <form>
-        <label>
-          <span>URL: </span>
-          <input onChange={handleChange} name="url" type="text" />
-          <button id="button" type="submit" onClick={handleSubmit}>
-            GO!
-          </button>
-        </label>
-        <label className="methods">
-          <button onClick={handleClick} id="get">
-            GET
-          </button>
-          <button onClick={handleClick} id="post">
-            POST
-          </button>
-          <button onClick={handleClick} id="put">
-            PUT
-          </button>
-          <button onClick={handleClick} id="delete">
-            DELETE
-          </button>
-        </label>
-      </form>
-      {textBox && (
-        <>
-          <textarea
-            name=""
-            onChange={handleData}
-            id="textarea"
-            cols="10"
-            rows="10"
-            placeholder='type JSON format for example {"content":"hello"}'
-          ></textarea>
-        </>
-      )}
-    </>
-  );
-}
+
+ 
+    return (
+      <>
+        <form onSubmit={handleSubmit}>
+          <label >
+            <span>URL: </span>
+            <input name='url' type='text' />
+            <button type="submit">GO!</button>
+          </label>
+          <label className="methods">
+            <span id="get">GET</span>
+            <span id="post">POST</span>
+            <span id="put">PUT</span>
+            <span id="delete">DELETE</span>
+          </label>
+        </form>
+      </>
+    );
+  }
 
 export default Form;
