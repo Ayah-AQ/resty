96ac95099f20d6e80201aada495117de56ef4dbd
Ayah-AQ <ayoya_1998@hotmail.com>
2023-08-27 23:26:42 +0000

    lab 28

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,82 @@
-import React from 'react';
-
-import './App.scss';
-
-// Let's talk about using index.js and some other name in the component folder
-// There's pros and cons for each way of doing this ...
-
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Form from './Components/Form';
-import Results from './Components/Results';
-import { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.scss";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Form from "./Components/Form";
+import Results from "./Components/Results";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [requestParams, setRequestParams] = useState({
+    method: "GET",
+    url: "",
+    body: "",
+  });
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState("");
 
-  const [data, setData] = useState(null);
-  const [requestParams, setRequestParams] = useState({});
+  useEffect(() => {
+    if (response) {
+      setData(JSON.stringify(response.data, null, 2));
+      setError(null);
+    }
+  }, [response]);
+
+  async function handleApiCall(requestParams) {
+    setData("");
+    setError(null);
+    setLoading(true);
   
-    function callApi(requestParams) {
-      const newData = {
-        results: [
-          // {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-          // {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-          {method:requestParams.method},
-          {url:requestParams.url},
-          {response:requestParams.data},
-        ],
+    try {
+      const requestOptions = {
+        method: requestParams.method,
+        url: requestParams.url,
+        data: requestParams.body,
       };
   
-      setData(newData);
-      setRequestParams(requestParams);
+      const apiResponse = await axios(requestOptions);
+      setResponse(apiResponse);
+    } catch (error) {
+      setLoading(false);
+  
+      if (axios.isAxiosError(error)) {
+        if (error.response) {
+          setError(
+            `${error.response.status} ${error.response.statusText}: ${error.response.data}`
+          );
+        } else {
+          setError('Network Error: Please check your internet connection.');
+        }
+      } else {
+        setError(error.message || 'An error occurred');
+      }
+    } finally {
+      setLoading(false);
     }
   
-    return (
-      <>
-        {/* {console.log(requestParams)} */}
-        <Header />
-        <div data-testid="req-method">Request Method: {requestParams.method}</div>
-        <div data-testid="url">URL: {requestParams.url}</div>
-        <Form handleApiCall={callApi} />
-        <Results data={data} />
-        <Footer />
-      </>
-    );
+    setRequestParams(requestParams);
   }
+  
+console.log(data)
+  return (
+    <React.Fragment>
+      <Header />
+      <div className="centered-container">
+        Request Method: {requestParams.method} <br />
+        URL: {requestParams.url}
+      </div>
+
+      <Form handleApiCall={handleApiCall} />
+      <Results
+        loading={loading}
+        error={error}
+        data={data}
+      />
+      <Footer />
+    </React.Fragment>
+  );
+}
+
 export default App;
